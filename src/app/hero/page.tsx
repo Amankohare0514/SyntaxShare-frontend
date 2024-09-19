c29aa9042b1422a433e31ce859a7af23fda007b5
Aman kohare <amankohare@gmail.com>
2024-09-19 20:17:25 +0530

    err

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -31,8 +31,20 @@ function Hero() {
             start sharing
           </button> */}
           <Link href={"/share"}>
-            <button className="bg-transparent border text-2xl hover:bg-[#BFB4FD] text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-              <svg className="w-8 h-8 mr-2" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#f0eaea" viewBox="0 0 24.00 24.00"><path d="M9.61109 12.4L10.8183 18.5355C11.0462 19.6939 12.6026 19.9244 13.1565 18.8818L19.0211 7.84263C19.248 7.41555 19.2006 6.94354 18.9737 6.58417M9.61109 12.4L5.22642 8.15534C4.41653 7.37131 4.97155 6 6.09877 6H17.9135C18.3758 6 18.7568 6.24061 18.9737 6.58417M9.61109 12.4L18.9737 6.58417M19.0555 6.53333L18.9737 6.58417" stroke="#000000" stroke-width="0.24000000000000005" /></svg>
+            <button className="bg-transparent border text-2xl hover:bg-[#C5B4FD] text-gray-800 dark:text-gray-200 font-bold py-2 px-4 rounded inline-flex items-center">
+              <svg
+                className="w-8 h-8 mr-2 text-gray-800 dark:text-gray-200"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M9.61109 12.4L10.8183 18.5355C11.0462 19.6939 12.6026 19.9244 13.1565 18.8818L19.0211 7.84263C19.248 7.41555 19.2006 6.94354 18.9737 6.58417M9.61109 12.4L5.22642 8.15534C4.41653 7.37131 4.97155 6 6.09877 6H17.9135C18.3758 6 18.7568 6.24061 18.9737 6.58417M9.61109 12.4L18.9737 6.58417M19.0555 6.53333L18.9737 6.58417"
+                  className="stroke-gray-800 dark:stroke-white"
+                  strokeWidth="0.24"
+                />
+              </svg>
               <span>Start Sharing</span>
             </button>
           </Link>
